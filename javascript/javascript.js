68000c9ed15daf3f9e489b9b8fcd737847bb50bc
William Yen <williamyen0203@gmail.com>
2015-12-30 19:34:45 -0500

    Add framework for accordion

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
-  $(".icon").tooltip({
-    position: {my: "center top", at: "center bottom"},
+  $('.icon').tooltip({
+    position: {my: 'center top', at: 'center bottom'},
     show: null,
-    hide: {effect: "slideUp", duration: 100},
+    hide: {effect: 'slideUp', duration: 100},
     open: function(event, ui){
-      ui.tooltip.animate({top: ui.tooltip.position().top + 5}, "fast");
+      ui.tooltip.animate({top: ui.tooltip.position().top + 5}, 'fast');
     }
   });
 
   // on-hover button text change
-  $(".button-container > form > input").hover(function(){
+  $('.button-container > form > input').hover(function(){
     if ($(this).val().length > 1) {
       $(this).val($(this).data('fa'));
-      $(this).css("font-family", "FontAwesome");
-      $(this).css("font-size", "20px");
+      $(this).css('font-family', 'FontAwesome');
+      $(this).css('font-size', '20px');
     } else {
-      $(this).val($(this).data("text"));
-      $(this).css("font-family", "inherit");
-      $(this).css("font-size", "inherit");
+      $(this).val($(this).data('text'));
+      $(this).css('font-family', 'inherit');
+      $(this).css('font-size', 'inherit');
     }
   });
 
@@ -25,18 +25,41 @@ $(document).ready(function(){
   var pageX = $(window).width();
   var pageY = $(window).height();
 
-  $("html").mousemove(function(e){
+  $('html').mousemove(function(e){
     var mouseX = e.pageX - pageX / 2;
     var mouseY = e.pageY - pageY / 2;
 
     var x = 50 + mouseX / 100;
     var y = 50 + mouseY / 100;
 
-    $(".bg-image").css("background-position", x + "% " + y + "%");
+    $('.bg-image').css('background-position', x + '% ' + y + '%');
   });
 
   // accordion
-  $(".image-container").on("click", function(){
-    console.log($(this));
+  var prevIndex = -1;
+
+  $('.image-container').on('click', function(){
+    var idName = $(this).attr('id');
+    var numCols = ($(window).width() < 768) ? 2 : ($(window).width() < 992) ? 3 : 4;
+    var index = $(this).index(".image-container");
+    var numElements = $('.image-container').length;
+    var lastElementOfRow = Math.floor(index / numCols) * numCols + numCols - 1;
+    lastElementOfRow = (lastElementOfRow < numElements) ? lastElementOfRow : numElements - 1;
+
+    console.log(lastElementOfRow);
+
+    if (prevIndex == index){
+      console.log("same one");
+    } else if (Math.floor(prevIndex / numCols) == Math.floor(index / numCols)){
+      console.log("same row");
+      $(".expand-temp").remove();
+      $('#' + idName + '-expand').clone().addClass('expand-temp').insertAfter($('.projects-container div.image-container:eq(' + lastElementOfRow +')')).show();
+    } else{
+      console.log("diff row");
+      $(".expand-temp").remove();
+      $('#' + idName + '-expand').clone().addClass('expand-temp').insertAfter($('.projects-container div.image-container:eq(' + lastElementOfRow +')')).show();
+    }
+
+    prevIndex = $(this).index(".image-container");
   });
 });
