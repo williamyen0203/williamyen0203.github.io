440b62e3617b4ed564b7869981a865d3fd8ab81c
William Yen <williamyen0203@gmail.com>
2016-01-01 14:34:20 -0500

    Position pointer

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,13 @@
+var prevIndex = -1;
+var numCols;
+var numElements;
+var pageX = $(window).width();
+var pageY = $(window).height();
+
 $(document).ready(function(){
+  //////////////////
+  // icon tooltip //
+  //////////////////
   $('.icon').tooltip({
     position: {my: 'center top', at: 'center bottom'},
     show: null,
@@ -8,7 +17,9 @@ $(document).ready(function(){
     }
   });
 
-  // on-hover button text change
+  /////////////////////////////////
+  // on-hover button text change //
+  /////////////////////////////////
   $('.button-container > form > input').hover(function(){
     if ($(this).val().length > 1) {
       $(this).val($(this).data('fa'));
@@ -21,10 +32,9 @@ $(document).ready(function(){
     }
   });
 
-  // mouse parallax
-  var pageX = $(window).width();
-  var pageY = $(window).height();
-
+  ////////////////////
+  // mouse parallax //
+  ////////////////////
   $('html').mousemove(function(e){
     var mouseX = e.pageX - pageX / 2;
     var mouseY = e.pageY - pageY / 2;
@@ -35,38 +45,29 @@ $(document).ready(function(){
     $('.bg-image').css('background-position', x + '% ' + y + '%');
   });
 
-  // accordion
-  var prevIndex = -1;
-
+  ///////////////
+  // accordion //
+  ///////////////
   $('.image-container').on('click', function(){
     var idName = $(this).attr('id');
-    var numCols = ($(window).width() < 768) ? 2 : ($(window).width() < 992) ? 3 : 4;
     var index = $(this).index(".image-container");
-    var numElements = $('.image-container').length;
     var lastElementOfRow = Math.floor(index / numCols) * numCols + numCols - 1;
     lastElementOfRow = (lastElementOfRow < numElements) ? lastElementOfRow : numElements - 1;
 
     if (prevIndex == index){
       console.log("same one");
-    } else if (Math.floor(prevIndex / numCols) == Math.floor(index / numCols)){
-      console.log("same row");
-
-      $(".expand-temp").html($('#' + idName + '-expand').html());
-      var newHeight = $(".expand-temp").height();
-      console.log(newHeight);
-
-      $('.expand-temp').animate({'height': 'auto'}, 'fast', function(){
-          $('.expand-temp').height('auto');//.html($('#' + idName + '-expand').html());
+      $(".expand-temp").slideUp("fast", function(){
+        $(".expand-temp").remove();
+        prevIndex = -1;
       });
-
-
-      // $(".expand-temp").slideUp("fast", function(){
-      //   $(".expand-temp").remove();
-      //   $('#' + idName + '-expand').clone().addClass('expand-temp').insertAfter($('.projects-container div.image-container:eq(' + lastElementOfRow +')')).slideDown().show();
-      // });
-
-      // $(".expand-temp").remove();
-      // $('#' + idName + '-expand').clone().addClass('expand-temp').insertAfter($('.projects-container div.image-container:eq(' + lastElementOfRow +')')).fadeIn().show();
+    // } else if (Math.floor(prevIndex / numCols) == Math.floor(index / numCols)){
+    //   console.log("same row");
+    //
+    //   $(".expand-temp").height(0);
+    //   $(".expand-temp").html($('#' + idName + '-expand').html());
+    //
+    //   $('.expand-temp').animate({'height': $('.expand-temp').get(0).scrollHeight}, 'fast');
+    //   $('.expand-temp').height($('.expand-temp').height() + 50);
     } else{
       console.log("diff row");
       if ($('.expand-temp').length > 0){
@@ -81,4 +82,35 @@ $(document).ready(function(){
 
     prevIndex = $(this).index(".image-container");
   });
+  ////////////////////////////////////
+  // initial positioning of pointer //
+  ////////////////////////////////////
+  positionPointer();
 });
+
+///////////////
+// on resize //
+///////////////
+$(window).on('resize orientationChange', function(){
+  positionPointer();
+  $(".expand-temp").slideUp("fast", function(){
+    $(".expand-temp").remove();
+  });
+});
+
+function positionPointer(){
+  numCols = ($(window).width() < 768) ? 2 : ($(window).width() < 992) ? 3 : 4;
+  numElements = $('.image-container').length;
+  var offset = $('.projects-container').width() / numCols;
+
+  $('.pointer').each(function(index){
+    $(this).css('left', (offset * (index % numCols)) + (offset / 2) - ($('.pointer').width() / 2) + 'px');
+
+    var diagonalHeight = getDiagonal($(this).height());
+    $(this).css('top', (getDiagonal($(this).height()) - $(this).height() / 2) - (($('.image-container').css('margin-bottom')).replace('px', '') * 0.75) + 'px');
+  });
+}
+
+function getDiagonal(side){
+  return (Math.sqrt(2 * side * side)) / 2;
+}
